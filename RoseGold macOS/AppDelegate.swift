5263d8b98d272f30883bf942fbfddb55d1f69ca2
Tatsuhiro Aoshima <hiro4bbh@gmail.com>
2019-01-12 13:33:32 +0900

    Initial Commit

---
+++ RoseGold macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  RoseGold macOS
+//
+//  Created by Tatsuhiro Aoshima on 2019/01/12.
+//  Copyright © 2019 Tatsuhiro Aoshima. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
